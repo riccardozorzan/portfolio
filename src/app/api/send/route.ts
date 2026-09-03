45d31d121f769dc14b24d75cda759d194eb6b672
Riccardo Zorzan <riccardo@riccardozorzan.com>
2026-09-04 01:28:35 +0200

    form email - resend, captcha

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: Request) {
     }
 
     const { data, error } = await resend.emails.send({
-      from: "Portfolio Contact Form <onboarding@resend.dev>",
+      from: "Riccardo Zorzan <noreply@riccardozorzan.com>",
       to: ["riccardo@riccardozorzan.com"],
       subject: `[Portfolio Transmission] Message from ${name}`,
       replyTo: cleanEmail,
