07edb6e8874cc8a8c42a408c18326f82f47b2cf3
Riccardo Zorzan <riccardo@riccardozorzan.com>
2026-09-04 10:59:26 +0200

    robots update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: [`${baseUrl}/sitemap.xml`],
   };
 }
